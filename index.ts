ea8d3d80a1a0e07399e07e119867fb68bbf2fbbc
うらあっくん <nowayoutbut@outlook.jp>
2019-09-09 17:15:16 +0900

    change

--- index.ts
+++ index.ts
@@ -2,9 +2,10 @@ import express from 'express';
 const port = Number(process.env.PORT) || 8080;
 
 const app = express();
+const env = process.env.NODE_ENV || 'Not Defined';
 
 app.get('/', (req, res) => {
-  res.send('Hello Google AppEngine with Typescript!');
+  res.send('<div>Hello Google AppEngine with Typescript!</div>' + `<div>This is ${env}</div>`);
 });
 
 app.listen(port, () => {
