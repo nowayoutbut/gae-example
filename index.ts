ba2f82dcbfadf4bc10df23fc0d697635656e9214
うらあっくん <nowayoutbut@outlook.jp>
2019-09-11 18:22:15 +0900

    環境情報を追加

--- index.ts
+++ index.ts
@@ -3,9 +3,14 @@ const port = Number(process.env.PORT) || 8080;
 
 const app = express();
 const env = process.env.NODE_ENV || 'Not Defined';
+const key = process.env.TWITTER_CONSUMER_KEY || 'Not Defined';
 
 app.get('/', (req, res) => {
-  res.send('<div>Hello Google AppEngine with Typescript!</div>' + `<div>This is ${env}</div>`);
+  res.send(`
+  <div>Hello Google AppEngine with Typescript!</div>
+  <div>This is ${env}</div>
+  <div>Twitter Consumer Key is ${key}</div>
+  `);
 });
 
 app.listen(port, () => {
